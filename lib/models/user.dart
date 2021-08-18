f269503f3494ccfa3e94c0efbc1dff622f4ada19
Tommy Chow <tommyxchow@gmail.com>
2021-08-18 14:01:40 -0400

    Add twitch (default) user model

---
+++ lib/models/user.dart
@@ -0,0 +1,23 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user.g.dart';
+
+// Twitch (default) user
+@JsonSerializable(createToJson: false, fieldRename: FieldRename.snake)
+class UserTwitch {
+  final String id;
+  final String login;
+  final String displayName;
+  final String type;
+  final String broadcasterType;
+  final String description;
+  final String profileImageUrl;
+  final String offlineImageUrl;
+  final String viewCount;
+  final String createdAt;
+
+  UserTwitch(this.id, this.login, this.displayName, this.type, this.broadcasterType, this.description, this.profileImageUrl, this.offlineImageUrl,
+      this.viewCount, this.createdAt);
+
+  factory UserTwitch.fromJson(Map<String, dynamic> json) => _$UserTwitchFromJson(json);
+}
---
+++ lib/models/user.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserTwitch _$UserTwitchFromJson(Map<String, dynamic> json) {
+  return UserTwitch(
+    json['id'] as String,
+    json['login'] as String,
+    json['display_name'] as String,
+    json['type'] as String,
+    json['broadcaster_type'] as String,
+    json['description'] as String,
+    json['profile_image_url'] as String,
+    json['offline_image_url'] as String,
+    json['view_count'] as String,
+    json['created_at'] as String,
+  );
+}
