31735244415426f26db57dd6a0a2e789e6194862
Tommy Chow <tommyxchow@gmail.com>
2021-08-20 21:16:59 -0400

    Add sample channels, http lib

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,53 @@
+import 'package:frosty/models/channel.dart';
+
+/// The twitch IRC websocket channel
 const twitchIrc = 'wss://irc-ws.chat.twitch.tv:443';
+
+/// Sample channel objects for testing/preview purposes.
+const SampleChannels = [
+  Channel(
+      id: '',
+      userId: '',
+      userLogin: 'xqcow',
+      userName: 'xQcOW',
+      gameId: '',
+      gameName: 'VALORANT',
+      type: 'live',
+      title: 'TITANIC MAN WITH UNREAL UNMATCHED UNBELIEVABLE STRENGHT AND APTITUDE ACCOMPLISHES ACHIEVEMENTS DEEMED IMPOSSIBLE BY THE PUBLIC (GONE WRONG)',
+      viewerCount: 76422,
+      startedAt: '',
+      language: 'en',
+      thumbnailUrl: 'https://static-cdn.jtvnw.net/previews-ttv/live_user_xqcow-{width}x{height}.jpg',
+      tagIds: [],
+      isMature: false),
+  Channel(
+      id: '',
+      userId: '',
+      userLogin: 'lirik',
+      userName: 'Lirik',
+      gameId: '',
+      gameName: 'Just Chatting',
+      type: 'live',
+      title: 'LOL',
+      viewerCount: 11933,
+      startedAt: '',
+      language: 'en',
+      thumbnailUrl: 'https://static-cdn.jtvnw.net/previews-ttv/live_user_lirik-{width}x{height}.jpg',
+      tagIds: [],
+      isMature: false),
+  Channel(
+      id: '',
+      userId: '',
+      userLogin: 'mizkif',
+      userName: 'Mizkif',
+      gameId: '',
+      gameName: 'Just Chatting',
+      type: 'live',
+      title: 'DRAMA DRAMA DRAMA',
+      viewerCount: 4547,
+      startedAt: '',
+      language: 'en',
+      thumbnailUrl: 'https://static-cdn.jtvnw.net/previews-ttv/live_user_mizkif-{width}x{height}.jpg',
+      tagIds: [],
+      isMature: false)
+];
