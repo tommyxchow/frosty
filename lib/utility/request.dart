254a60bb492cdc38f7b5fab8d26b0d817a42ddf9
Tommy Chow <tommyxchow@gmail.com>
2021-08-23 15:43:52 -0400

    Add basic error handling to requests & cleanup

--- lib/utility/request.dart
+++ lib/utility/request.dart
@@ -11,36 +11,36 @@ class Request {
   static Future<Map<String, String>> getEmotesBTTVGlobal() async {
     final url = Uri.parse('https://api.betterttv.net/3/cached/emotes/global');
     final response = await http.get(url);
-    final decoded = jsonDecode(response.body) as List;
-    final List<EmoteBTTVGlobal> emotes = decoded.map((emote) => EmoteBTTVGlobal.fromJson(emote)).toList();
 
-    final emoteToUrl = <String, String>{};
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body) as List;
+      final List<EmoteBTTVGlobal> emotes = decoded.map((emote) => EmoteBTTVGlobal.fromJson(emote)).toList();
 
-    for (var emote in emotes) {
-      emoteToUrl[emote.code] = 'https://cdn.betterttv.net/emote/${emote.id}/3x';
+      final emoteToUrl = <String, String>{};
+      emotes.forEach((emote) => emoteToUrl[emote.code] = 'https://cdn.betterttv.net/emote/${emote.id}/3x');
+
+      return emoteToUrl;
+    } else {
+      throw Exception(['Failed to get global BTTV emotes', 'Error code: ${response.statusCode}']);
     }
-    return emoteToUrl;
   }
 
   /// Returns a mapping of a channel's BTTV emotes to their URL.
   static Future<Map<String, String>> getEmotesBTTVChannel({required String id}) async {
     final url = Uri.parse('https://api.betterttv.net/3/cached/users/twitch/$id');
     final response = await http.get(url);
+
     if (response.statusCode == 200) {
       final decoded = jsonDecode(response.body);
       final result = EmoteBTTVChannel.fromJson(decoded);
 
       final emoteToUrl = <String, String>{};
+      result.channelEmotes.forEach((emote) => emoteToUrl[emote.code] = 'https://cdn.betterttv.net/emote/${emote.id}/3x');
+      result.sharedEmotes.forEach((emote) => emoteToUrl[emote.code] = 'https://cdn.betterttv.net/emote/${emote.id}/3x');
 
-      for (final emote in result.channelEmotes) {
-        emoteToUrl[emote.code] = 'https://cdn.betterttv.net/emote/${emote.id}/3x';
-      }
-      for (final emote in result.sharedEmotes) {
-        emoteToUrl[emote.code] = 'https://cdn.betterttv.net/emote/${emote.id}/3x';
-      }
       return emoteToUrl;
     } else {
-      return {};
+      throw Exception(['Failed to get BTTV emotes for id: $id', 'Error code: ${response.statusCode}']);
     }
   }
 
@@ -48,30 +48,36 @@ class Request {
   static Future<Map<String, String>> getEmotesFFZGlobal() async {
     final url = Uri.parse('https://api.betterttv.net/3/cached/frankerfacez/emotes/global');
     final response = await http.get(url);
-    final decoded = jsonDecode(response.body) as List;
-    final List<EmoteFFZ> emotes = decoded.map((emote) => EmoteFFZ.fromJson(emote)).toList();
 
-    final emoteToUrl = <String, String>{};
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body) as List;
+      final List<EmoteFFZ> emotes = decoded.map((emote) => EmoteFFZ.fromJson(emote)).toList();
 
-    for (var emote in emotes) {
-      emoteToUrl[emote.code] = emote.images.url4x ?? emote.images.url1x;
+      final emoteToUrl = <String, String>{};
+      emotes.forEach((emote) => emoteToUrl[emote.code] = emote.images.url4x ?? emote.images.url1x);
+
+      return emoteToUrl;
+    } else {
+      throw Exception(['Failed to get global FFZ emotes', 'Error code: ${response.statusCode}']);
     }
-    return emoteToUrl;
   }
 
   /// Returns a mapping of a channel's FFZ emotes to their URL.
   static Future<Map<String, String>> getEmotesFFZChannel({required String id}) async {
     final url = Uri.parse('https://api.betterttv.net/3/cached/frankerfacez/users/twitch/$id');
     final response = await http.get(url);
-    final decoded = jsonDecode(response.body) as List;
-    final List<EmoteFFZ> emotes = decoded.map((emote) => EmoteFFZ.fromJson(emote)).toList();
 
-    final emoteToUrl = <String, String>{};
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body) as List;
+      final List<EmoteFFZ> emotes = decoded.map((emote) => EmoteFFZ.fromJson(emote)).toList();
 
-    for (var emote in emotes) {
-      emoteToUrl[emote.code] = emote.images.url4x ?? emote.images.url1x;
+      final emoteToUrl = <String, String>{};
+      emotes.forEach((emote) => emoteToUrl[emote.code] = emote.images.url4x ?? emote.images.url1x);
+
+      return emoteToUrl;
+    } else {
+      throw Exception(['Failed to get FFZ emotes for id: $id', 'Error code: ${response.statusCode}']);
     }
-    return emoteToUrl;
   }
 
   /// Returns a mapping of global Twitch emotes to their URL.
@@ -79,15 +85,18 @@ class Request {
     final url = Uri.parse('https://api.twitch.tv/helix/chat/emotes/global');
     final headers = {'Authorization': 'Bearer $token', 'Client-Id': const String.fromEnvironment('CLIENT_ID')};
     final response = await http.get(url, headers: headers);
-    final decoded = jsonDecode(response.body)['data'] as List;
-    final List<EmoteTwitch> emotes = decoded.map((emote) => EmoteTwitch.fromJson(emote)).toList();
 
-    final emoteToUrl = <String, String>{};
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body)['data'] as List;
+      final List<EmoteTwitch> emotes = decoded.map((emote) => EmoteTwitch.fromJson(emote)).toList();
 
-    for (var emote in emotes) {
-      emoteToUrl[emote.name] = 'https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0';
+      final emoteToUrl = <String, String>{};
+      emotes.forEach((emote) => emoteToUrl[emote.name] = 'https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0');
+
+      return emoteToUrl;
+    } else {
+      throw Exception(['Failed to get global Twitch emotes', 'Error code: ${response.statusCode}']);
     }
-    return emoteToUrl;
   }
 
   /// Returns a mapping of a channel's BTTV emotes to their URL.
@@ -95,50 +104,61 @@ class Request {
     final url = Uri.parse('https://api.twitch.tv/helix/chat/emotes?broadcaster_id=$id');
     final headers = {'Authorization': 'Bearer $token', 'Client-Id': const String.fromEnvironment('CLIENT_ID')};
     final response = await http.get(url, headers: headers);
-    final decoded = jsonDecode(response.body)['data'] as List;
-    final List<EmoteTwitch> emotes = decoded.map((emote) => EmoteTwitch.fromJson(emote)).toList();
 
-    final emoteToUrl = <String, String>{};
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body)['data'] as List;
+      final List<EmoteTwitch> emotes = decoded.map((emote) => EmoteTwitch.fromJson(emote)).toList();
 
-    for (var emote in emotes) {
-      emoteToUrl[emote.name] = 'https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0';
+      final emoteToUrl = <String, String>{};
+      emotes.forEach((emote) => emoteToUrl[emote.name] = 'https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0');
+
+      return emoteToUrl;
+    } else {
+      throw Exception(['Failed to get Twitch emotes for id: $id', 'Error code: ${response.statusCode}']);
     }
-    return emoteToUrl;
   }
 
   static Future<Map<String, String>> getBadgesTwitchGlobal({required String token}) async {
     final url = Uri.parse('https://api.twitch.tv/helix/chat/badges/global');
     final headers = {'Authorization': 'Bearer $token', 'Client-Id': const String.fromEnvironment('CLIENT_ID')};
     final response = await http.get(url, headers: headers);
-    final decoded = jsonDecode(response.body)['data'] as List;
-    final List<BadgesTwitch> badges = decoded.map((emote) => BadgesTwitch.fromJson(emote)).toList();
 
-    final badgeToUrl = <String, String>{};
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body)['data'] as List;
+      final List<BadgesTwitch> badges = decoded.map((emote) => BadgesTwitch.fromJson(emote)).toList();
 
-    for (final badge in badges) {
-      for (final badgeVersion in badge.versions) {
-        badgeToUrl['${badge.setId}/${badgeVersion.id}'] = badgeVersion.imageUrl4x;
+      final badgeToUrl = <String, String>{};
+      for (final badge in badges) {
+        for (final badgeVersion in badge.versions) {
+          badgeToUrl['${badge.setId}/${badgeVersion.id}'] = badgeVersion.imageUrl4x;
+        }
       }
+
+      return badgeToUrl;
+    } else {
+      throw Exception(['Failed to get global Twitch badges', 'Error code: ${response.statusCode}']);
     }
-    return badgeToUrl;
   }
 
   static Future<Map<String, String>> getBadgesTwitchChannel({required String token, required String id}) async {
     final url = Uri.parse('https://api.twitch.tv/helix/chat/badges?broadcaster_id=$id');
     final headers = {'Authorization': 'Bearer $token', 'Client-Id': const String.fromEnvironment('CLIENT_ID')};
     final response = await http.get(url, headers: headers);
-    final decoded = jsonDecode(response.body)['data'] as List;
-    final List<BadgesTwitch> badges = decoded.map((emote) => BadgesTwitch.fromJson(emote)).toList();
 
-    final badgeToUrl = <String, String>{};
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body)['data'] as List;
+      final List<BadgesTwitch> badges = decoded.map((emote) => BadgesTwitch.fromJson(emote)).toList();
 
-    for (final badge in badges) {
-      for (final badgeVersion in badge.versions) {
-        badgeToUrl['${badge.setId}/${badgeVersion.id}'] = badgeVersion.imageUrl4x;
+      final badgeToUrl = <String, String>{};
+      for (final badge in badges) {
+        for (final badgeVersion in badge.versions) {
+          badgeToUrl['${badge.setId}/${badgeVersion.id}'] = badgeVersion.imageUrl4x;
+        }
       }
+
+      return badgeToUrl;
+    } else {
+      throw Exception(['Failed to get Twitch badges for id: $id', 'Error code: ${response.statusCode}']);
     }
-    return badgeToUrl;
   }
 }
-
-enum HTTPMethod { GET, POST }
