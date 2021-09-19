84b8eb16761f3776aeddc7d7dcbf0968e2ca532e
Tommy Chow <tommyxchow@gmail.com>
2021-09-19 18:25:39 -0400

    Run build_runner

--- lib/models/badges.g.dart
+++ lib/models/badges.g.dart
@@ -6,20 +6,17 @@ part of 'badges.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-BadgeImagesTwitch _$BadgeImagesTwitchFromJson(Map<String, dynamic> json) {
-  return BadgeImagesTwitch(
-    json['id'] as String,
-    json['image_url_1x'] as String,
-    json['image_url_2x'] as String,
-    json['image_url_4x'] as String,
-  );
-}
+BadgeImagesTwitch _$BadgeImagesTwitchFromJson(Map<String, dynamic> json) =>
+    BadgeImagesTwitch(
+      json['id'] as String,
+      json['image_url_1x'] as String,
+      json['image_url_2x'] as String,
+      json['image_url_4x'] as String,
+    );
 
-BadgesTwitch _$BadgesTwitchFromJson(Map<String, dynamic> json) {
-  return BadgesTwitch(
-    json['set_id'] as String,
-    (json['versions'] as List<dynamic>)
-        .map((e) => BadgeImagesTwitch.fromJson(e as Map<String, dynamic>))
-        .toList(),
-  );
-}
+BadgesTwitch _$BadgesTwitchFromJson(Map<String, dynamic> json) => BadgesTwitch(
+      json['set_id'] as String,
+      (json['versions'] as List<dynamic>)
+          .map((e) => BadgeImagesTwitch.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
