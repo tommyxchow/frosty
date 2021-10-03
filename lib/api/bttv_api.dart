8c2038f92d4bca1ecfd6cc9053f64b54935a85b6
Tommy Chow <tommyxchow@gmail.com>
2021-10-03 18:08:51 -0400

    Refactor requests to their own dedicated API files

---
+++ lib/api/bttv_api.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:frosty/models/emotes.dart';
+import 'package:http/http.dart' as http;
+
+class BTTV {
+  /// Returns a map of global BTTV emotes to their URL.
+  static Future<Map<String, String>?> getEmotesGlobal() async {
+    final url = Uri.parse('https://api.betterttv.net/3/cached/emotes/global');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body) as List;
+      final List<EmoteBTTVGlobal> emotes = decoded.map((emote) => EmoteBTTVGlobal.fromJson(emote)).toList();
+
+      final emoteToUrl = <String, String>{};
+      for (final emote in emotes) {
+        emoteToUrl[emote.code] = 'https://cdn.betterttv.net/emote/${emote.id}/3x';
+      }
+
+      return emoteToUrl;
+    } else {
+      debugPrint('Failed to get global BTTV emotes. Error code: ${response.statusCode}');
+    }
+  }
+
+  /// Returns a map of a channel's BTTV emotes to their URL.
+  static Future<Map<String, String>?> getEmotesChannel({required String id}) async {
+    final url = Uri.parse('https://api.betterttv.net/3/cached/users/twitch/$id');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body);
+      final result = EmoteBTTVChannel.fromJson(decoded);
+
+      final emoteToUrl = <String, String>{};
+      for (final emote in result.channelEmotes) {
+        emoteToUrl[emote.code] = 'https://cdn.betterttv.net/emote/${emote.id}/3x';
+      }
+      for (final emote in result.sharedEmotes) {
+        emoteToUrl[emote.code] = 'https://cdn.betterttv.net/emote/${emote.id}/3x';
+      }
+
+      return emoteToUrl;
+    } else {
+      debugPrint('Failed to get BTTV emotes for id: $id. Error code: ${response.statusCode}');
+    }
+  }
+}
---
+++ lib/api/ffz_api.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:frosty/models/emotes.dart';
+import 'package:http/http.dart' as http;
+
+class FFZ {
+  /// Returns a map of global FFZ emotes to their URL.
+  static Future<Map<String, String>?> getEmotesGlobal() async {
+    final url = Uri.parse('https://api.betterttv.net/3/cached/frankerfacez/emotes/global');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body) as List;
+      final List<EmoteFFZ> emotes = decoded.map((emote) => EmoteFFZ.fromJson(emote)).toList();
+
+      final emoteToUrl = <String, String>{};
+      for (final emote in emotes) {
+        emoteToUrl[emote.code] = emote.images.url4x ?? emote.images.url1x;
+      }
+
+      return emoteToUrl;
+    } else {
+      debugPrint('Failed to get global FFZ emotes. Error code: ${response.statusCode}');
+    }
+  }
+
+  /// Returns a map of a channel's FFZ emotes to their URL.
+  static Future<Map<String, String>?> getEmotesChannel({required String id}) async {
+    final url = Uri.parse('https://api.betterttv.net/3/cached/frankerfacez/users/twitch/$id');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body) as List;
+      final List<EmoteFFZ> emotes = decoded.map((emote) => EmoteFFZ.fromJson(emote)).toList();
+
+      final emoteToUrl = <String, String>{};
+      for (final emote in emotes) {
+        emoteToUrl[emote.code] = emote.images.url4x ?? emote.images.url1x;
+      }
+
+      return emoteToUrl;
+    } else {
+      debugPrint('Failed to get FFZ emotes for id: $id. Error code: ${response.statusCode}');
+    }
+  }
+}
