41af97341ed671878393c3bf38d0cb3b442f3efb
Tommy Chow <tommyxchow@gmail.com>
2021-10-12 02:31:32 -0400

    Convert ChannelList to stateless widget

--- lib/screens/channel_list.dart
+++ lib/screens/channel_list.dart
@@ -3,42 +3,32 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:frosty/stores/channel_list_store.dart';
 import 'package:frosty/widgets/channel_card.dart';
 
-class ChannelList extends StatefulWidget {
+class ChannelList extends StatelessWidget {
   final ChannelCategory category;
   final ChannelListStore channelListStore;
 
   const ChannelList({Key? key, required this.category, required this.channelListStore}) : super(key: key);
 
   @override
-  _ChannelListState createState() => _ChannelListState();
-}
-
-class _ChannelListState extends State<ChannelList> with AutomaticKeepAliveClientMixin<ChannelList> {
-  @override
   Widget build(BuildContext context) {
     debugPrint('build channel list');
-    super.build(context);
 
     return RefreshIndicator(
       child: Observer(
         builder: (_) {
-          final channels = widget.channelListStore.channels(category: widget.category);
           return ListView.builder(
-            itemCount: channels.length,
+            itemCount: channelListStore.channels(category: category).length,
             padding: const EdgeInsets.all(5.0),
             itemBuilder: (context, index) {
-              if (index > channels.length / 2 && widget.channelListStore.hasMore(category: widget.category)) {
-                widget.channelListStore.getChannels(category: widget.category);
+              if (index > channelListStore.channels(category: category).length / 2 && channelListStore.hasMore(category: category)) {
+                channelListStore.getChannels(category: category);
               }
-              return ChannelCard(channelInfo: channels.elementAt(index));
+              return ChannelCard(channelInfo: channelListStore.channels(category: category).elementAt(index));
             },
           );
         },
       ),
-      onRefresh: () => widget.channelListStore.refresh(category: widget.category),
+      onRefresh: () => channelListStore.refresh(category: category),
     );
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
