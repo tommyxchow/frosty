c6a6fd72be463fb4c74534924299831cd40ff2dd
tommyxchow <tommyxchow@gmail.com>
2022-01-07 23:12:41 -0500

    Polish chat settings message preview (#65, #67)

--- lib/screens/settings/chat_settings.dart
+++ lib/screens/settings/chat_settings.dart
@@ -1,15 +1,25 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:frosty/screens/settings/stores/settings_store.dart';
 import 'package:frosty/widgets/section_header.dart';
 
-class ChatSettings extends StatelessWidget {
+class ChatSettings extends StatefulWidget {
   final SettingsStore settingsStore;
 
   const ChatSettings({Key? key, required this.settingsStore}) : super(key: key);
 
   @override
+  _ChatSettingsState createState() => _ChatSettingsState();
+}
+
+class _ChatSettingsState extends State<ChatSettings> {
+  var showPreview = false;
+
+  @override
   Widget build(BuildContext context) {
+    final settingsStore = widget.settingsStore;
+
     return Observer(
       builder: (context) {
         return Column(
@@ -47,27 +57,47 @@ class ChatSettings extends StatelessWidget {
               value: settingsStore.useReadableColors,
               onChanged: (newValue) => settingsStore.useReadableColors = newValue,
             ),
-            const SizedBox(height: 20),
-            ListTile(
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Hello! This is a text preview message.',
-                    textScaleFactor: settingsStore.fontScale,
-                  ),
-                  SizedBox(height: settingsStore.messageSpacing),
-                  Text(
-                    'This is a another message to visualize spacing!',
-                    textScaleFactor: settingsStore.fontScale,
-                  ),
-                ],
-              ),
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 200),
+              child: showPreview
+                  ? Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(20.0),
+                      margin: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 10.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purpleAccent),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text.rich(
+                            TextSpan(children: [
+                              const TextSpan(text: 'Hello! This is a text preview message '),
+                              WidgetSpan(
+                                alignment: PlaceholderAlignment.middle,
+                                child: CachedNetworkImage(
+                                  imageUrl: 'https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/3.0',
+                                  placeholder: (context, url) => const SizedBox(),
+                                  height: 30.0,
+                                ),
+                              )
+                            ]),
+                            textScaleFactor: settingsStore.fontScale,
+                          ),
+                          SizedBox(height: settingsStore.messageSpacing),
+                          Text(
+                            'Here is a another message to visualize spacing!',
+                            textScaleFactor: settingsStore.fontScale,
+                          ),
+                        ],
+                      ),
+                    )
+                  : const SizedBox(),
             ),
-            const SizedBox(height: 10),
             ListTile(
               isThreeLine: true,
-              title: Text('Font scale: ${settingsStore.fontScale.toStringAsFixed(1)}x'),
+              title: Text('Message scale: ${settingsStore.fontScale.toStringAsFixed(1)}x'),
               subtitle: Slider.adaptive(
                 value: settingsStore.fontScale,
                 min: 0.5,
@@ -84,6 +114,14 @@ class ChatSettings extends StatelessWidget {
                 max: 30.0,
                 divisions: 30,
                 onChanged: (newValue) => settingsStore.messageSpacing = newValue,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              width: double.infinity,
+              child: OutlinedButton(
+                onPressed: () => setState(() => showPreview = !showPreview),
+                child: Text(showPreview ? 'Hide Text Preview' : 'Show Text Preview'),
               ),
             ),
           ],
