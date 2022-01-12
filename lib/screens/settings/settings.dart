b35fcc2fa420ea4048330b4ff1081d12a8c64a86
Tommy Chow <tommyxchow@gmail.com>
2022-01-11 19:50:59 -0500

    Add repo link to settings

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:frosty/core/auth/auth_store.dart';
-import 'package:frosty/screens/settings/account_settings.dart';
-import 'package:frosty/screens/settings/chat_settings.dart';
-import 'package:frosty/screens/settings/general_settings.dart';
-import 'package:frosty/screens/settings/other_settings.dart';
+import 'package:frosty/screens/settings/sections/account_settings.dart';
+import 'package:frosty/screens/settings/sections/chat_settings.dart';
+import 'package:frosty/screens/settings/sections/general_settings.dart';
+import 'package:frosty/screens/settings/sections/other_settings.dart';
+import 'package:frosty/screens/settings/sections/video_settings.dart';
 import 'package:frosty/screens/settings/stores/settings_store.dart';
-import 'package:frosty/screens/settings/video_settings.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Settings extends StatelessWidget {
   final SettingsStore settingsStore;
@@ -18,6 +20,13 @@ class Settings extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Settings'),
+        actions: [
+          IconButton(
+            tooltip: 'View source on GitHub',
+            onPressed: () => launch('https://github.com/tommyxchow/frosty'),
+            icon: const FaIcon(FontAwesomeIcons.github),
+          ),
+        ],
       ),
       body: SafeArea(
         bottom: false,
