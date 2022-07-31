49b1b5277db377fc9c1ce087ebd6e9b85d446d49
Tommy Chow <tommyxchow@gmail.com>
2022-07-31 16:39:54 -0400

    Remove unused channel fields

--- lib/models/channel.dart
+++ lib/models/channel.dart
@@ -8,21 +8,11 @@ class Channel {
   final String broadcasterId;
   final String broadcasterLogin;
   final String broadcasterName;
-  final String broadcasterLanguage;
-  final String gameId;
-  final String gameName;
-  final String title;
-  final int delay;
 
   const Channel({
     required this.broadcasterId,
     required this.broadcasterLogin,
     required this.broadcasterName,
-    required this.broadcasterLanguage,
-    required this.gameId,
-    required this.gameName,
-    required this.title,
-    required this.delay,
   });
 
   factory Channel.fromJson(Map<String, dynamic> json) => _$ChannelFromJson(json);
@@ -31,29 +21,17 @@ class Channel {
 // Object for Twitch channel search query.
 @JsonSerializable(createToJson: false, fieldRename: FieldRename.snake)
 class ChannelQuery {
-  final String broadcasterLanguage;
   final String broadcasterLogin;
   final String displayName;
-  final String gameId;
-  final String gameName;
   final String id;
   final bool isLive;
-  final List<String> tagIds;
-  final String thumbnailUrl;
-  final String title;
   final String startedAt;
 
   const ChannelQuery({
-    required this.broadcasterLanguage,
     required this.broadcasterLogin,
     required this.displayName,
-    required this.gameId,
-    required this.gameName,
     required this.id,
     required this.isLive,
-    required this.tagIds,
-    required this.thumbnailUrl,
-    required this.title,
     required this.startedAt,
   });
 
