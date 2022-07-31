dac0dcd0829f0a93773889a3a497610c8e0d4a41
Tommy Chow <tommyxchow@gmail.com>
2022-07-31 16:41:07 -0400

    Update tests

--- test/stream_test.dart
+++ test/stream_test.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frosty/models/stream.dart';
 
@@ -30,19 +31,14 @@ void main() {
     final decoded = jsonDecode(sampleStream);
     final stream = StreamTwitch.fromJson(decoded);
 
-    expect(stream.id, '43809710301');
     expect(stream.userId, '71092938');
     expect(stream.userLogin, 'xqcow');
     expect(stream.userName, 'xQcOW');
     expect(stream.gameId, '508292');
     expect(stream.gameName, 'Bloons TD 6');
-    expect(stream.type, 'live');
     expect(stream.title, 'A shorter title...');
     expect(stream.viewerCount, 50051);
     expect(stream.startedAt, '2021-09-23T20:06:22Z');
-    expect(stream.language, 'en');
     expect(stream.thumbnailUrl, 'https://static-cdn.jtvnw.net/previews-ttv/live_user_xqcow-{width}x{height}.jpg');
-    expect(stream.tagIds, ['e6bb8b34-4c28-4b5f-94ed-12c1ebf2d0e4', '6ea6bca4-4712-4ab9-a906-e3336a9d8039', '6606e54c-f92d-40f6-8257-74977889ccdd']);
-    expect(stream.isMature, false);
   });
 }
