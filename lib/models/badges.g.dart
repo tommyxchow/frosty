f17cef952326687b89b473835dd1ef59f2bc3bde
Tommy <tommyxchow@gmail.com>
2023-04-11 08:52:18 -0400

    Migrate to new Twitch badges endpoint (#256)

--- lib/models/badges.g.dart
+++ lib/models/badges.g.dart
@@ -11,7 +11,9 @@ BadgeInfoTwitch _$BadgeInfoTwitchFromJson(Map<String, dynamic> json) =>
       json['image_url_1x'] as String,
       json['image_url_2x'] as String,
       json['image_url_4x'] as String,
+      json['id'] as String,
       json['title'] as String,
+      json['description'] as String,
     );
 
 BadgeInfoFFZ _$BadgeInfoFFZFromJson(Map<String, dynamic> json) => BadgeInfoFFZ(
