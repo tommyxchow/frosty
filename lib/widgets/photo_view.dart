a4039f18202bd1cb9ea65d103bdee5a442b53206
Tommy Chow <tommyxchow@gmail.com>
2024-08-11 01:41:19 -0700

    Add dismissible

--- lib/widgets/photo_view.dart
+++ lib/widgets/photo_view.dart
@@ -2,26 +2,40 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 
-class FrostyPhotoViewDialog extends StatelessWidget {
+class FrostyPhotoViewDialog extends StatefulWidget {
   final String imageUrl;
 
   const FrostyPhotoViewDialog({super.key, required this.imageUrl});
 
   @override
+  State<FrostyPhotoViewDialog> createState() => _FrostyPhotoViewDialogState();
+}
+
+class _FrostyPhotoViewDialogState extends State<FrostyPhotoViewDialog> {
+  PhotoViewScaleState photoViewScaleState = PhotoViewScaleState.initial;
+
+  @override
   Widget build(BuildContext context) {
     return Stack(
-      alignment: Alignment.bottomCenter,
+      alignment: Alignment.topCenter,
       children: [
-        PhotoView(
-          imageProvider: CachedNetworkImageProvider(imageUrl),
-          backgroundDecoration: const BoxDecoration(color: Colors.transparent),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 32),
-          child: IconButton(
-            icon: const Icon(Icons.close, color: Colors.white),
-            onPressed: Navigator.of(context).pop,
+        Dismissible(
+          key: const Key('photo_view_dismissible'),
+          direction: photoViewScaleState == PhotoViewScaleState.initial
+              ? DismissDirection.vertical
+              : DismissDirection.none,
+          onDismissed: Navigator.of(context).pop,
+          child: PhotoView(
+            imageProvider: CachedNetworkImageProvider(widget.imageUrl),
+            scaleStateChangedCallback: (value) =>
+                setState(() => photoViewScaleState = value),
+            backgroundDecoration:
+                const BoxDecoration(color: Colors.transparent),
           ),
+        ),
+        IconButton(
+          icon: const Icon(Icons.close, color: Colors.white),
+          onPressed: Navigator.of(context).pop,
         ),
       ],
     );
