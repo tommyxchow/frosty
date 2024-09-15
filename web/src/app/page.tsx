e383ed35a2cc3d723d2c6396f34798ffc232bc04
Tommy Chow <tommyxchow@gmail.com>
2024-09-14 20:17:30 -0700

    add emote marquee

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,7 +4,12 @@ import screenshotFollowing from '@/assets/screenshot-following.png';
 import screenshotSearch from '@/assets/screenshot-search.png';
 import { FeatureCard } from '@/components/FeatureCard';
 import { appStoreLink, playStoreLink } from '@/lib/constants';
+import Image from 'next/image';
+import Marquee from 'react-fast-marquee';
 import { SiApple, SiGoogleplay } from 'react-icons/si';
+
+const NUM_ROWS = 10;
+const NUM_COLUMNS = 40;
 
 export default function Home() {
   const coreFeatures = [
@@ -60,16 +65,35 @@ export default function Home() {
   return (
     <article className='flex flex-col gap-8 sm:gap-16'>
       <section className='mt-24 flex flex-col items-center gap-8 sm:gap-16'>
-        <div className='flex w-full justify-center rounded-2xl border border-neutral-300 p-8 dark:border-neutral-900'>
-          <video
-            className='max-h-[800px] rounded-xl border border-neutral-300 object-contain dark:border-neutral-900'
-            src='/video.webm'
-            autoPlay
-            loop
-            muted
-            playsInline
-            disableRemotePlayback
-          />
+        <div className='grid w-full items-center rounded-2xl border border-neutral-300 dark:border-neutral-900 [&>*]:col-start-1 [&>*]:row-start-1'>
+          <div className='flex flex-col gap-4'>
+            {Array.from({ length: NUM_ROWS }).map((_, index) => (
+              <Marquee key={index} direction={index % 2 ? 'left' : 'right'}>
+                {Array.from({ length: NUM_COLUMNS }).map((_, index) => (
+                  <Image
+                    key={index}
+                    width={32}
+                    height={32}
+                    alt='pepeD'
+                    unoptimized
+                    src='https://cdn.7tv.app/emote/6072a16fdcae02001b44e614/4x.webp'
+                  />
+                ))}
+              </Marquee>
+            ))}
+          </div>
+
+          <div className='z-10 justify-self-center p-8'>
+            <video
+              className='max-h-[800px] rounded-xl border border-neutral-300 object-contain dark:border-neutral-900'
+              src='/video.webm'
+              autoPlay
+              loop
+              muted
+              playsInline
+              disableRemotePlayback
+            />
+          </div>
         </div>
 
         <div className='flex flex-col gap-8'>
