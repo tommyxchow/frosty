e00c93289c59782bf593f07ce221c2d6a0746db6
Tommy Chow <tommyxchow@gmail.com>
2025-08-23 12:21:41 -0700

    optimize profile picture fetching fixes refetches on tab switch

--- lib/widgets/profile_picture.dart
+++ lib/widgets/profile_picture.dart
@@ -4,7 +4,7 @@ import 'package:frosty/models/user.dart';
 import 'package:frosty/widgets/cached_image.dart';
 import 'package:provider/provider.dart';
 
-class ProfilePicture extends StatelessWidget {
+class ProfilePicture extends StatefulWidget {
   final String userLogin;
   final double radius;
 
@@ -15,22 +15,62 @@ class ProfilePicture extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    final diameter = radius * 2;
+  State<ProfilePicture> createState() => _ProfilePictureState();
+}
 
+class _ProfilePictureState extends State<ProfilePicture> {
+  // Cache profile image URLs to avoid repeated API calls
+  static final Map<String, String> _urlCache = {};
+  static final Map<String, Future<String>> _pendingRequests = {};
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<String> _getProfileImageUrl() async {
+    final userLogin = widget.userLogin;
+    
+    // Return cached URL if available
+    if (_urlCache.containsKey(userLogin)) {
+      return _urlCache[userLogin]!;
+    }
+    
+    // Return existing pending request if one is already in progress
+    if (_pendingRequests.containsKey(userLogin)) {
+      return _pendingRequests[userLogin]!;
+    }
+    
+    // Make new request and cache it
+    final future = context.read<TwitchApi>().getUser(userLogin: userLogin)
+        .then((user) => user.profileImageUrl);
+    _pendingRequests[userLogin] = future;
+    
+    try {
+      final url = await future;
+      _urlCache[userLogin] = url;
+      _pendingRequests.remove(userLogin);
+      return url;
+    } catch (e) {
+      _pendingRequests.remove(userLogin);
+      rethrow;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final diameter = widget.radius * 2;
     final placeholderColor = Theme.of(context).colorScheme.surfaceContainer;
 
     return ClipOval(
-      child: FutureBuilder(
-        future: context.read<TwitchApi>().getUser(
-              userLogin: userLogin,
-            ),
-        builder: (context, AsyncSnapshot<UserTwitch> snapshot) {
+      child: FutureBuilder<String>(
+        future: _getProfileImageUrl(),
+        builder: (context, snapshot) {
           return snapshot.hasData
               ? FrostyCachedNetworkImage(
                   width: diameter,
                   height: diameter,
-                  imageUrl: snapshot.data!.profileImageUrl,
+                  imageUrl: snapshot.data!,
                   placeholder: (context, url) =>
                       ColoredBox(color: placeholderColor),
                 )
@@ -42,5 +82,12 @@ class ProfilePicture extends StatelessWidget {
         },
       ),
     );
+  }
+
+  // Clear cache when app needs fresh data (e.g., user logout/login)
+  // Can be called from anywhere: ProfilePicture.clearCache();
+  static void clearCache() {
+    _urlCache.clear();
+    _pendingRequests.clear();
   }
 }
